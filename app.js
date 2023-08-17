d5d18bd9aa7c48bdd2b33d45684b38b85d6a6aa0
Rudra Patel <rudracode2004@gmail.com>
2023-08-17 20:23:55 +0530

    initial commit

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
     "https://i-cart-frontend.vercel.app"
   );
   res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
