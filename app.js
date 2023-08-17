da45762e71a3d6aef0585b8883c8a4aa9429df89
Rudra Patel <rudracode2004@gmail.com>
2023-08-17 20:52:15 +0530

    klajadjojvEQ

--- app.js
+++ app.js
@@ -25,15 +25,15 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Access-Control-Allow-Origin",
-//     "https://i-cart-frontend.vercel.app"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://i-cart-frontend.vercel.app"
+  );
+  // res.setHeader("Access-Control-Allow-Credentials", "true");
+  // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use(cors({ credentials: true, optionsSuccessStatus: 200 }));
 
