41a15dab0252073168e30fd33b3e01d4a28fa334
Rudra Patel <rudracode2004@gmail.com>
2023-08-18 17:15:48 +0530

    cors is still a shit

--- app.js
+++ app.js
@@ -20,47 +20,26 @@ config({
 
 const app = express();
 
+const allowedOrigins = ["https://i-cart-frontend.vercel.app"];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 // USING MIDDLEWARES
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://i-cart-frontend.vercel.app"
-  );
-  // res.setHeader("Access-Control-Allow-Credentials", "true");
-  // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
-app.use(
-  cors({
-    origin: "https://i-cart-frontend.vercel.app",
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
-);
-
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       const allowedOrigins = [
-//         process.env.FRONTEND_URL,
-//         process.env.FRONTEND_URL_2,
-//         process.env.FRONTEND_URL_3,
-//       ];
-//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//     optionsSuccessStatus: 200,
-//   })
-// );
 
 app.get("/api/v1/order/getkey", (req, res) => {
   res.status(200).json({ key: process.env.RAZORPAY_API_KEY });
