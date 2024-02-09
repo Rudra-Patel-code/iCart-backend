da1bead200774cee5789444c58e380ad3c80bca1
Rudra Patel <rudracode2004@gmail.com>
2024-02-09 21:25:52 +0530

    added cors -100

--- app.js
+++ app.js
@@ -15,86 +15,27 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 config({
-  path: "./config/config.env",
+    path: "./config/config.env",
 });
 
 const app = express();
 
+app.use(
+    cors({
+        origin:
+            process.env.CORS_ORIGIN === "*"
+                ? "*" // This might give CORS error for some origins due to credentials set to true
+                : process.env.CORS_ORIGIN?.split(","), // For multiple cors origin for production. Refer https://github.com/hiteshchoudhary/apihub/blob/a846abd7a0795054f48c7eb3e71f3af36478fa96/.env.sample#L12C1-L12C12
+        credentials: true,
+    })
+);
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const allowedOrigins = [
-  "https://i-cart-frontend.vercel.app",
-  "https://icart-frontend.onrender.com",
-];
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://i-cart-frontend.vercel.app"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
-// app.use(
-//   cors({
-//     origin: "https://i-cart-frontend.vercel.app",
-//     credentials: true,
-//     optionsSuccessStatus: 200,
-//   })
-// );
-
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       const allowedOrigins = [
-//         process.env.FRONTEND_URL,
-//         process.env.FRONTEND_URL_2,
-//         process.env.FRONTEND_URL_3,
-//       ];
-//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//     optionsSuccessStatus: 200,
-//   })
-// );
-
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true,
-//   optionsSuccessStatus: 200,
-// };
-
-// USING MIDDLEWARES
 
 app.get("/api/v1/order/getkey", (req, res) => {
-  res.status(200).json({ key: process.env.RAZORPAY_API_KEY });
+    res.status(200).json({ key: process.env.RAZORPAY_API_KEY });
 });
 // ROUTES
 app.use("/api/v1/auth", authRouter);
@@ -106,6 +47,6 @@ app.use("/api/v1/order", orderRouter);
 export default app;
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"));
+    res.sendFile(path.join(__dirname, "/index.html"));
 });
 app.use(ErrorMiddlware);
